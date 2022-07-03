b015ba583d8cddf2ab5ae2f700a561b3854cdbbf
Daniel <daniel@insite.com>
2022-07-03 10:45:32 -0600

    First commit

---
+++ FileScanner.cs
@@ -0,0 +1,33 @@
+﻿internal class FileScanner
+{
+    public int Touch(string directory, string pattern)
+    {
+        int count = 0;
+
+        DirectoryInfo di = new(directory);
+        FileSystemInfo[] files = di.GetFileSystemInfos(pattern);
+        foreach (FileSystemInfo file in files)
+        {
+            string filetype = ((file.Attributes & FileAttributes.Directory) == 0) ? "file   " : "folder ";
+
+            Console.Write("Touching " + filetype + file.FullName);
+
+            file.LastWriteTime = DateTime.Now;
+
+            Console.Write(" OK\n");
+            count++;
+        }
+
+        DirectoryInfo[] dirs = di.GetDirectories();
+        foreach (DirectoryInfo dir in dirs)
+        {
+            int temp = Touch(dir.FullName, pattern);
+            if (temp >= 0)
+                count += temp;
+            else
+                return -1;
+        }
+
+        return count;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿if (Validate(args))
+    Execute(Environment.CurrentDirectory, args[0]);
+
+bool Validate(string[] args)
+{
+    if (args.Length == 1)
+        return true;
+
+    Console.WriteLine("\nUpdates the Last Modified date and time on all files in a folder, including its subfolders.\n");
+    Console.WriteLine("          Usage :: TouchFiles pattern\n");
+    Console.WriteLine("        pattern :: File name search pattern (e.g., *.xyz)\n");
+    return false;
+}
+
+void Execute(string directory, string pattern)
+{
+    try
+    {
+        Console.WriteLine("Directory = " + directory);
+        Console.WriteLine("  Pattern = " + pattern);
+
+        var scanner = new FileScanner();
+        int count = scanner.Touch(directory, pattern);
+
+        if (count == 0)
+            Console.WriteLine("No files found.");
+        else if (count > 0)
+            Console.WriteLine("{0} object(s) touched successfully.", count);
+        else
+            Console.WriteLine("Errors found - operation aborted!");
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine("\nFatal error occured: {0}", e.ToString());
+    }
+}
